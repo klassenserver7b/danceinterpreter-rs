f981219b1181cc313a80715865671ede176dd535
Konsl <82901383+Konsl@users.noreply.github.com>
2025-03-11 00:25:59 +0100

    move server code to separate file

--- src/traktor_api/server.rs
+++ src/traktor_api/server.rs
@@ -1,10 +1,43 @@
 use crate::async_utils::DroppingOnce;
 use crate::traktor_api::model::{AppMessage, ServerMessage};
+use iced::futures::channel::mpsc::{UnboundedReceiver, UnboundedSender};
 use iced::futures::channel::{mpsc, oneshot};
 use iced::futures::stream;
 use iced::futures::{SinkExt, Stream, StreamExt};
 use std::sync::{Arc, Mutex};
 use warp::Filter;
+
+async fn server_main(
+    port: u16,
+    mut output: UnboundedSender<ServerMessage>,
+    mut input: UnboundedReceiver<AppMessage>,
+    cancelled: oneshot::Receiver<()>,
+) {
+    let test = Arc::new(Mutex::new(false));
+    let test_clone = Arc::clone(&test);
+
+    let hello = warp::path!("hello" / String)
+        .map(move |name| format!("Hello, {}! {}", name, test_clone.lock().unwrap()));
+
+    println!("starting traktor server on port {}", port);
+    let Ok((_, fut)) =
+        warp::serve(hello).try_bind_with_graceful_shutdown(([127, 0, 0, 1], port), async {
+            cancelled.await.ok();
+        })
+    else {
+        println!("could not start traktor server on port {}", port);
+        return;
+    };
+    tokio::task::spawn(fut);
+
+    loop {
+        match input.select_next_some().await {
+            AppMessage::Reconnect { debug_logging } => {
+                *test.lock().unwrap() = debug_logging;
+            }
+        }
+    }
+}
 
 pub fn run_server(port: u16) -> impl Stream<Item = ServerMessage> {
     let (mut output, output_receive) = mpsc::unbounded();
@@ -12,33 +45,10 @@ pub fn run_server(port: u16) -> impl Stream<Item = ServerMessage> {
 
     let runner = DroppingOnce::new(
         async move {
-            let (input_send, mut input) = mpsc::unbounded();
+            let (input_send, input) = mpsc::unbounded();
             let _ = output.send(ServerMessage::Ready(input_send)).await;
 
-            let test = Arc::new(Mutex::new(false));
-            let test_clone = Arc::clone(&test);
-
-            let hello = warp::path!("hello" / String)
-                .map(move |name| format!("Hello, {}! {}", name, test_clone.lock().unwrap()));
-
-            println!("starting traktor server on port {}", port);
-            let Ok((_, fut)) =
-                warp::serve(hello).try_bind_with_graceful_shutdown(([127, 0, 0, 1], port), async {
-                    cancelled.await.ok();
-                })
-            else {
-                println!("could not start traktor server on port {}", port);
-                return;
-            };
-            tokio::task::spawn(fut);
-
-            loop {
-                match input.select_next_some().await {
-                    AppMessage::Reconnect { debug_logging } => {
-                        *test.lock().unwrap() = debug_logging;
-                    }
-                }
-            }
+            server_main(port, output, input, cancelled).await
         },
         move || {
             println!("stopping traktor server on port {}", port);
