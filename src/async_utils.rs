b8687c0ba1b9d84ba62ee7396a4001cfe86dea1f
Konsl <82901383+Konsl@users.noreply.github.com>
2025-03-11 12:39:01 +0100

    move Runner struct to run_subscription_with function

--- src/async_utils.rs
+++ src/async_utils.rs
@@ -68,33 +68,6 @@ impl<Fut: Future, DropFn: FnOnce() -> ()> FusedStream for DroppingOnce<Fut, Drop
     }
 }
 
-struct Runner<I, F, S, T>
-where
-    F: FnOnce(&I, EventStream) -> S,
-    S: Stream<Item = T>,
-{
-    data: I,
-    spawn: F,
-}
-
-impl<I, F, S, T> Recipe for Runner<I, F, S, T>
-where
-    I: Hash + 'static,
-    F: FnOnce(&I, EventStream) -> S,
-    S: Stream<Item = T> + MaybeSend + 'static,
-{
-    type Output = T;
-
-    fn hash(&self, state: &mut Hasher) {
-        std::any::TypeId::of::<I>().hash(state);
-        self.data.hash(state);
-    }
-
-    fn stream(self: Box<Self>, input: EventStream) -> BoxStream<Self::Output> {
-        boxed_stream((self.spawn)(&self.data, input))
-    }
-}
-
 // TODO: replace with Subscription::run_with once the update comes out
 pub fn run_subscription_with<T, D, S>(data: D, builder: fn(&D) -> S) -> Subscription<T>
 where
@@ -102,6 +75,33 @@ where
     S: Stream<Item = T> + MaybeSend + 'static,
     T: 'static,
 {
+    struct Runner<I, F, S, T>
+    where
+        F: FnOnce(&I, EventStream) -> S,
+        S: Stream<Item = T>,
+    {
+        data: I,
+        spawn: F,
+    }
+
+    impl<I, F, S, T> Recipe for Runner<I, F, S, T>
+    where
+        I: Hash + 'static,
+        F: FnOnce(&I, EventStream) -> S,
+        S: Stream<Item = T> + MaybeSend + 'static,
+    {
+        type Output = T;
+
+        fn hash(&self, state: &mut Hasher) {
+            std::any::TypeId::of::<I>().hash(state);
+            self.data.hash(state);
+        }
+
+        fn stream(self: Box<Self>, input: EventStream) -> BoxStream<Self::Output> {
+            boxed_stream((self.spawn)(&self.data, input))
+        }
+    }
+
     from_recipe(Runner {
         data: (data, builder),
         spawn: |(data, builder), _| builder(data),
