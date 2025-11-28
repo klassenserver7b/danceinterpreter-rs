349e31d541d6ac94190f1daf0779905579ea875d
klassenserver7b <79657220+klassenserver7b@users.noreply.github.com>
2025-11-28 23:50:29 +0100

    update actions

--- traktor-api/cover-loader/build.sh
+++ traktor-api/cover-loader/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-bun build --compile --target=bun-linux-x64 ./index.ts --outfile target/cover-loader
-bun build --compile --target=bun-windows-x64-baseline ./index.ts --outfile target/cover-loader
+bun build --compile --target=bun-linux-x64 ./index.ts --outfile target/cover-loader-linux-x64
+bun build --compile --target=bun-linux-aarch64 ./index.ts --outfile target/cover-loader-linux-aarch64
+bun build --compile --target=bun-windows-x64-baseline ./index.ts --outfile target/cover-loader-windows-x64
 
